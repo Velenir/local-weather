d60405d61d262176bc3f97cd201e8867445e9edb
Velenir <Velenir@users.noreply.github.com>
2016-10-14 23:19:09 +0300

    Added an icon for wind direction

--- app/components/Location.jsx
+++ app/components/Location.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Location = ({location}) => <p className="location__display">{location.full}</p>;
 
-const WStation = ({location}) => <p className="location__observation"><small>measured at</small> {location.city}</p>;
+const WStation = ({location}) => <p className="location__observation"><small>as measured at</small> {location.city}</p>;
 
 export default ({display_location, observation_location}) => (
 	<div className="location">
--- app/components/Wind.jsx
+++ app/components/Wind.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
 import {bemify} from '../helpers/bemify';
+import windsock from '../images/windsock.min.svg';
 
-export default ({wind_dir, wind_mph, wind_degrees, cls}) => (
-	<div className={bemify(cls, "--wind")}>
-		<p className="title">Wind</p>
-		<p className="reading">{wind_dir} {wind_mph} mph</p>
-	</div>
-);
+export default class Wind extends React.Component {
+	componentDidMount() {
+		// do a lesser arc
+		const degree = this.props.wind_degrees > 180 ? 360 - this.props.wind_degrees : this.props.wind_degrees;
+
+		setTimeout(() => this.icon.style.transform = `rotate(${degree}deg)`, 0);
+	}
+
+	render() {
+		const {wind_dir, wind_mph, cls} = this.props;
+		console.log(this.props.wind_degrees);
+
+		return (
+			<div className={bemify(cls, "--wind")}>
+				<p className="title">Wind</p>
+				<div className="reading">
+					<img src={windsock} alt="windsock" className="reading__icon" ref={c => this.icon = c} style={{transition: "transform 2s cubic-bezier(0.28, 0.07, 0.44, 1.9)"}}/>
+					<span className="reading__value"> {wind_dir} {wind_mph} mph</span>
+				</div>
+			</div>
+		);
+	}
+}
