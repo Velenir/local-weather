7489cd98db35e8e5742d0e25f93a9d1cf9bc4943
Velenir <Velenir@users.noreply.github.com>
2016-10-23 21:56:16 +0300

    Updated DenomController

--- app/helpers/tempDegrees.js
+++ app/helpers/tempDegrees.js
@@ -3,36 +3,31 @@ const DEGREES = {CELSIUS: "°C", FAHRENHEIT: "°F"};
 class DenomController {
 	constructor() {
 		this.denom = DEGREES.CELSIUS;
-		this.components = [];
+		this.components = new Set();
 	}
 
 	switchDenom = () => {
 		console.log("switching degrees");
 		this.denom = this.denom === DEGREES.CELSIUS ? DEGREES.FAHRENHEIT : DEGREES.CELSIUS;
 
-		for (let c of this.components) {
-			c.setState({
-				denom: this.denom
-			});
-		}
+		this.components.forEach(c => c.setState({
+			denom: this.denom
+		}));
 	}
 
 	addComponent(c) {
-		this.components.push(c);
+		this.components.add(c);
 	}
 
 	removeComponent(c) {
-		const ind = this.components.indexOf(c);
-		if(ind === -1) return;
-
-		this.components.splice(ind, 1);
+		this.components.delete(c);
 	}
 
 	clearComponents() {
-		this.components = [];
+		this.components.clear();
 	}
 }
 
-const denomController = new DenomController;
+const denomController = new DenomController();
 
 export {DEGREES, denomController};
