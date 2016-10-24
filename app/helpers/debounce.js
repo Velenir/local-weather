ace0a2d743e9daa3b32ec505231568710e34928e
Velenir <Velenir@users.noreply.github.com>
2016-10-24 22:15:37 +0300

    Added cancel token for debounced function

--- app/helpers/debounce.js
+++ app/helpers/debounce.js
@@ -1,4 +1,4 @@
-export default function debounce(func, wait, immediate) {
+export default function debounce(func, wait, immediate, token) {
 	var timeout;
 	return function() {
 		var context = this, args = arguments;
@@ -9,6 +9,7 @@ export default function debounce(func, wait, immediate) {
 		var callNow = immediate && !timeout;
 		clearTimeout(timeout);
 		timeout = setTimeout(later, wait);
+		if (token) token.cancel = clearTimeout.bind(null, timeout);
 		if (callNow) func.apply(context, args);
 	};
 }
