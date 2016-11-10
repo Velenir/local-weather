900b8f55aecae81e029cf08026f9643fcaaccd0a
Velenir <Velenir@users.noreply.github.com>
2016-11-10 22:32:59 +0300

    Simplified error display

--- app/components/Location.jsx
+++ app/components/Location.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Location = ({location}) => <p className="location__display">{location.full}</p>;
 
 const WStation = ({location, error}) => error ?
-	(<p className="location__observation location__observation--error">Error {error.type}: {error.description}</p>) :
+	(<p className="location__observation location__observation--error">{error.description}</p>) :
 	(<p className="location__observation"><small>weather as measured at</small> {location.city}</p>);
 
 export default ({display_location, observation_location, children, response}) => (
