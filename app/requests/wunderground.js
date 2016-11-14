e343ff736cd6a8d7930c866178698c8af05ab396
Velenir <Velenir@users.noreply.github.com>
2016-11-14 19:23:16 +0300

    Match mixed content

--- app/requests/wunderground.js
+++ app/requests/wunderground.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getWeather(apiKey, query, ...features) {
 	// console.log("REQUESTING:", `https://api.wunderground.com/api/${apiKey}/${features.join("/")}/q/${query}.json`);
-	return axios.get(`https://api.wunderground.com/api/${apiKey}/${features.join("/")}/q/${query}.json`);
+	return axios.get(`//api.wunderground.com/api/${apiKey}/${features.join("/")}/q/${query}.json`);
 }
 
 export function autocomplete(query, options = {}) {
